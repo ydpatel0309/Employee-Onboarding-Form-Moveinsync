8ffbbbcd4ab4782cdbec27a8ffb780e615b447ff
Yash Patel <91671051+ydpatel0309@users.noreply.github.com>
2024-02-13 20:12:27 +0530

    added save and reset button and made reponsive also

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ function App() {
     <>
     <div className="App">
     
-      <div className="App-inner">
+      <div className="App_inner">
       <img src={logo} alt="logo" id='moveinsync_logo'/>
       <div className="profile">
-        
+        <div className="profile_heading">
+          <div className="heading"><h1>Employee Profile</h1></div>
+          <div className="buttons">
+            <button id="Save_button" type="submit">Save</button>
+            <button id="Reset_button" type="reset">Reset</button>
+          </div>
+        </div>
       </div>
         
       </div>
