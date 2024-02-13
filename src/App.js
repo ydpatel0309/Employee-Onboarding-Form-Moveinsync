376142ee360d92cafb1757778dc9da1dcc3598d9
Yash Patel <91671051+ydpatel0309@users.noreply.github.com>
2024-02-13 19:44:37 +0530

    implemented a moveinsync logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./images/logo.png"
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div className="App-inner">
+      <img src={logo} alt="logo" id='moveinsync_logo'/>
+      <div className="profile">
+        
+      </div>
+        
+      </div>
     </div>
+    </>
   );
 }
 
